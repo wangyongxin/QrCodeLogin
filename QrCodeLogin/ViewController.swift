a71121bfe4b1d46c3620976153cac1d34a91007c
wangyongxin <wangyongxin546@163.com>
2019-06-03 16:00:35 +0800

    Initial Commit

---
+++ QrCodeLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QrCodeLogin
+//
+//  Created by 王勇欣 on 2019/6/3.
+//  Copyright © 2019 titan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
